126f34ab9a22500e1a458a7e44aa2bac8bfc619b
kawata15 <kkw.bsk-r@outlook.com>
2018-12-21 21:27:27 +0900

    グループのモデル、コントローラ作成
    DB設計時、membersテーブルとしていたがgroup_userでコントローラとモデルを作成しrake db:migrateを行なってしまった
    rails dでgroup_userを削除
    さらにdb:rollbackした
    rails g でmember作成後db:migrateするもエラー出たのでマスターブランチに戻り一度ブランチ削除
    再度ブランチ作成後グループ等作成
    DB上にgroupテーブルが２つになってしまったのでシークエルプロ から古いものを削除した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_21_041722) do
+ActiveRecord::Schema.define(version: 2018_12_21_114728) do
+
+  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name"
+  end
+
+  create_table "gruops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_gruops_on_name"
+  end
+
+  create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_members_on_group_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +48,6 @@ ActiveRecord::Schema.define(version: 2018_12_21_041722) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "members", "groups"
+  add_foreign_key "members", "users"
 end
