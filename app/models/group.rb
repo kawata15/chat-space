019d098e6bc1e8595b109c8e87c9c72469ea911b
kawata15 <kkw.bsk-r@outlook.com>
2018-12-24 15:07:06 +0900

    サイドバーに最新メッセが表示される
    DBのmessgesテーブルのcontentカラムの最後に入ったものを取得するメソッドを定義

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,13 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :users, through: :members
   validates :name, presence: true
+
+  def show_last_message                         #最新メッセージの表示
+    if (last_message = messages.last).present?  #（ラストメッセ定義）.あれば値を返す
+      last_message.content? ? last_message.content : '画像が投稿されています' #三項演算子でさらに分岐
+    else                                        #配列が空の場合
+      'まだメッセージはありません。'
+    end
+  end
+
 end
